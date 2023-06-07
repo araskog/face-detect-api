7613cbacf8f2a21168c85af01f71fab1bf1bec72
Amanda Araskog <amanda.araskog@normative.io>
2023-06-07 18:15:35 +0200

    Return Clarifai errors

--- controllers/image.js
+++ controllers/image.js
@@ -7,23 +7,24 @@ const app = new Clarifai.App({
 
 const handleApiCall = (req, res) => {
   app.models
-    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
-    .then(data => {
+    .predict(Clarifai.FACE_DETECTION_MODEL, req.body.input)
+    .then((data) => {
       res.json(data);
     })
-    .catch(err => res.status(400).json('unable to work with API'))
+    .catch((err) => res.status(400).json(err));
 }
 
 
  const handleImage = (req, res, db) => {
 	const { id } = req.body;
-	db('users').where('id', '=', id)
-	.increment('entries', 1)
-	.returning('entries')
-	.then(entries => {
-		res.json(entries[0])
-	})
-	.catch(err => res.status(400).json('Unable to get entries'))
+	db("users")
+    .where("id", "=", id)
+    .increment("entries", 1)
+    .returning("entries")
+    .then((entries) => {
+      res.json(entries[0]);
+    })
+    .catch((err) => res.status(400).json(err));
 }
 
 module.exports = {
