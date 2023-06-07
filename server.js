20cd47495b66ea78b36e4e3798a7713aed1cbff6
Amanda Araskog <amanda.araskog@normative.io>
2023-06-07 17:09:50 +0200

    Add db environment variables

--- server.js
+++ server.js
@@ -10,14 +10,19 @@ const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 
 const db = knex({
-	client: 'pg',
-	connection: {
-		connectionString: process.env.DATABASE_URL,
-		ssl: {
-			rejectUnauthorized: false
-		}
-	}
-})
+  client: "pg",
+  connection: {
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+    host: process.env.DATABASE_HOST,
+    port: 5432,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PW,
+    database: process.env.DATABASE_DB,
+  },
+});
 
 const saltRounds = 10;
 const app = express();
